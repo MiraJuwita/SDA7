50285b89f9ae22b3bb9cfa9dbd594f7c0ab3cc42
MiraJuwita <98795964+MiraJuwita@users.noreply.github.com>
2022-05-11 12:31:56 +0700

    Update G1A021056_MiraJuwitaAli_QueueAlternative2.cpp

--- G1A021056_MiraJuwitaAli_QueueAlternative2.cpp
+++ G1A021056_MiraJuwitaAli_QueueAlternative2.cpp
@@ -31,19 +31,19 @@ Algoritma
 			Q.head <- Q.head + 1
 			Q.tail <- Q.tail + 1
 			Q.info[Q.tail] <- P
-	else if ( Q.tail = NMax-1 ) then   			// Kondisi Khusus, jika tail ada di ujung kanan, dan tidak penuh
-			i ? Q.head         					// maka seluruh antrian dimajukan ke posisi paling depan semua
+	else if ( Q.tail = NMax-1 ) then   	      // Kondisi Khusus, jika tail ada di ujung kanan, dan tidak penuh
+			i ? Q.head         	      // maka seluruh antrian dimajukan ke posisi paling depan semua
 			j ? 0
 			while ( i < Q.tail ) do 
 					Q.info[j] <- Q.[i] 
 					i <- i + 1
 					j <- j + 1
 			{end while}
-			Q.head <- 0          			  // maka head menjadi 0 dan tail sama dengan j
+			Q.head <- 0          		// maka head menjadi 0 dan tail sama dengan j
 			Q.tail <- j
-			Q.info[Q.tail] <- P  			  // data di enqueue di posisi tail
+			Q.info[Q.tail] <- P  		// data di enqueue di posisi tail
 	else
-		Q.tail <- Q.tail + 1      	         // kondisi biasa 
+		Q.tail <- Q.tail + 1      	        // kondisi biasa 
 		Q.info[Q.tail] <- P 
 	{end if}
 
@@ -55,13 +55,13 @@ Procedure DeQueueAlt2(input/output Q : queue)
   FS. Queue di-dequeue , dengan Alternative 2 }
 Kamus
 Algoritma
-	if ( Q.head = -1 and Q.Tail = -1 ) then		//queue kosong
+	if ( Q.head = -1 and Q.Tail = -1 ) then	//queue kosong
 		  output('Stack kosong')	
-	else	              						// queue tidak kosong
-      p <- Q.info[q.head]						// data yang di head disimpan di p
-      if ( Q.head = Q.tail ) then				// jika 1 elemen dan posisi dimanapun dan tidak kosong
-        Q.head <- -1							//maka head dan tail pasti jadi -1 semua} Q.tail   -1
-      else										// kondisi biasa, headnya nambah 1
+	else	              			// queue tidak kosong
+      p <- Q.info[q.head]			// data yang di head disimpan di p
+      if ( Q.head = Q.tail ) then		// jika 1 elemen dan posisi dimanapun dan tidak kosong
+        Q.head <- -1				//maka head dan tail pasti jadi -1 semua} Q.tail   -1
+      else					// kondisi biasa, headnya nambah 1
         Q.head ++
       {end if}
 	{end if}    
